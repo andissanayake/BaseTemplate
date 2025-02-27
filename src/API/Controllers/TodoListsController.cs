dfc687341b26b76cbfdc92f1578bd77bd7470e7c
Akalanka Dissanayake <connect.dissanayake@gmail.com>
2025-02-27 23:10:58 +0800

    init set up

---
+++ src/API/Controllers/TodoListsController.cs
@@ -0,0 +1,45 @@
+﻿using BaseTemplate.Application.TodoLists.Commands.CreateTodoList;
+using BaseTemplate.Application.TodoLists.Commands.DeleteTodoList;
+using BaseTemplate.Application.TodoLists.Commands.UpdateTodoList;
+using BaseTemplate.Application.TodoLists.Queries.GetTodos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaseTemplate.API.Controllers;
+
+[Authorize]
+public class TodoListsController : ApiControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<TodosVm>> Get()
+    {
+        return await Mediator.Send(new GetTodosQuery());
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<int>> Create(CreateTodoListCommand command)
+    {
+        return await Mediator.Send(command);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult> Update(int id, UpdateTodoListCommand command)
+    {
+        if (id != command.Id)
+        {
+            return BadRequest();
+        }
+
+        await Mediator.Send(command);
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> Delete(int id)
+    {
+        await Mediator.Send(new DeleteTodoListCommand(id));
+
+        return NoContent();
+    }
+}
---
+++ src/API/Services/CurrentUserService.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Claims;
+
+using BaseTemplate.Application.Common.Interfaces;
+
+namespace BaseTemplate.API.Services;
+
+public class CurrentUserService : IUser
+{
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public CurrentUserService(IHttpContextAccessor httpContextAccessor)
+    {
+        _httpContextAccessor = httpContextAccessor;
+    }
+
+    public string? Id => _httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier);
+}
