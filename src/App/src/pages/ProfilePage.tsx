23e697aa910941d16ef55a408f10a7ce72ee0c7e
Akalanka Dissanayake <connect.dissanayake@gmail.com>
2025-03-29 14:59:11 +0800

    secure componnet to show case protected

--- src/App/src/pages/ProfilePage.tsx
+++ src/App/src/pages/ProfilePage.tsx
@@ -1,7 +1,13 @@
 import { useAuthStore } from "../auth/authStore";
+import { Secure } from "../auth/Secure";
 
 export const ProfilePage = () => {
   const user = useAuthStore((state) => state.user);
 
-  return <div>{user?.displayName}</div>;
+  return (
+    <div>
+      {user?.displayName}
+      <Secure />
+    </div>
+  );
 };
