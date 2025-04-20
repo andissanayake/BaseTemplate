6e74c2141842e7b3cd6c360ee611fa449f70dffa
Akalanka Dissanayake <connect.dissanayake@gmail.com>
2025-04-20 20:36:43 +0800

    refactor

--- src/Application/Common/RequestHandler/ServiceCollectionExtensions.cs
+++ src/Application/Common/RequestHandler/ServiceCollectionExtensions.cs
@@ -10,7 +10,7 @@ public static class ServiceCollectionExtensions
         services.AddScoped<IMediator, Mediator>();
         var handlerTypes = assembly.GetTypes()
             .Where(t => !t.IsAbstract && !t.IsInterface)
-            .SelectMany(t => t.GetInterfaces(), (t, i) => new { t, i })
+            .SelectMany(t => t.GetInterfaces(), (t, i) => new { Implementation = t, Interface = i })
             .Where(x =>
                 x.i.IsGenericType &&
                 x.i.GetGenericTypeDefinition() == typeof(IRequestHandler<,>))
