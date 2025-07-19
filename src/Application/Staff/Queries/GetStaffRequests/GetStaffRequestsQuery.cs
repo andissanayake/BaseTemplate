c9eb5d9d04a09c6d0a684b6dde78bd7d3367cdcf
Akalanka Dissanayake <connect.dissanayake@gmail.com>
2025-07-19 16:37:10 +0800

    staff request

--- src/Application/Staff/Queries/GetStaffRequests/GetStaffRequestsQuery.cs
+++ src/Application/Staff/Queries/GetStaffRequests/GetStaffRequestsQuery.cs
@@ -1,3 +1,3 @@
 namespace BaseTemplate.Application.Staff.Queries.GetStaffRequests;
-[Authorize(Roles = Roles.StaffManager)]
+[Authorize(Roles = Roles.StaffRequestManager)]
 public record GetStaffRequestsQuery() : IRequest<List<StaffRequestDto>>;
