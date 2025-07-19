d8f7d4336457a14e85353733f25e2cec09c651b6
Akalanka Dissanayake <connect.dissanayake@gmail.com>
2025-07-19 14:20:08 +0800

    app breadcrumb fixes

--- src/App/src/layout/AppBreadcrumb.tsx
+++ src/App/src/layout/AppBreadcrumb.tsx
@@ -4,19 +4,21 @@ import { Link, useLocation, matchPath } from "react-router-dom";
 
 export const AppBreadcrumb = () => {
   const breadcrumbRoutes: Record<string, string | ((params: any) => string)> = {
-    "/": "Home",
     "/todo-list": "Todo List",
     "/todo-list/create": "Create",
     "/todo-list/edit/:listId": (params: any) => `Edit #${params.listId}`,
     "/todo-list/view/:listId": (params: any) => `View #${params.listId}`,
+    //application
+    "/": "Home",
+    "/tenants/create": "Create Tenant",
+    "/tenants/view": "Tenant View",
+    "/tenants/edit": "Tenant Edit",
+
     "/items": "Items",
     "/items/create": "Create Item",
     "/items/edit/:itemId": (params: any) => `Edit Item #${params.itemId}`,
     "/items/view/:itemId": (params: any) => `View Item #${params.itemId}`,
     "/profile": "Profile",
-    "/tenants/create": "Create Tenant",
-    "/tenants/view": () => ` Tenant View`,
-    "/tenants/edit": () => ` Tenant Edit`,
     "/tenants/view/:tenantId/staff-requests": "Staff Requests",
     "/tenants/view/:tenantId/staff": "Staff Management",
   };
