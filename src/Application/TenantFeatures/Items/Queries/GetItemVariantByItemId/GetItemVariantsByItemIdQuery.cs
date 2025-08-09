30f60f2995854e01e4a7a80ddb73cfa07f8a7647
Akalanka Dissanayake <connect.dissanayake@gmail.com>
2025-08-09 21:47:35 +0800

    rename fix

--- src/Application/TenantFeatures/Items/Queries/GetItemVariantByItemId/GetItemVariantsByItemIdQuery.cs
+++ src/Application/TenantFeatures/Items/Queries/GetItemVariantByItemId/GetItemVariantsByItemIdQuery.cs
@@ -1,4 +1,4 @@
 namespace BaseTemplate.Application.TenantFeatures.Items.Queries.GetItemVariantByItemId;
 
 [Authorize(Roles = Roles.ItemManager)]
-public record GetItemVariantsByItemIdQuery(int ItemId) : IRequest<List<ItemVariantDto>> { }
+public record GetItemVariantByItemIdQuery(int ItemId) : IRequest<List<ItemVariantDto>> { }
