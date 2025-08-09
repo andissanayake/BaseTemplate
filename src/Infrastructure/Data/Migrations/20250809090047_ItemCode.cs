5b77c504c6b76950b4b8ac7203c92ff78a59a008
Akalanka Dissanayake <connect.dissanayake@gmail.com>
2025-08-09 17:01:11 +0800

    item code

---
+++ src/Infrastructure/Data/Migrations/20250809090047_ItemCode.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace BaseTemplate.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ItemCode : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Code",
+                table: "Item",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "ItemVariant",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ItemId = table.Column<int>(type: "integer", nullable: false),
+                    Code = table.Column<string>(type: "text", nullable: false),
+                    Price = table.Column<decimal>(type: "numeric", nullable: false),
+                    Created = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    CreatedBy = table.Column<int>(type: "integer", nullable: false),
+                    LastModified = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    LastModifiedBy = table.Column<int>(type: "integer", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false),
+                    TenantId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ItemVariant", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ItemVariant_Item_ItemId",
+                        column: x => x.ItemId,
+                        principalTable: "Item",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ItemVariant_Tenant_TenantId",
+                        column: x => x.TenantId,
+                        principalTable: "Tenant",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ItemVariantCharacteristic",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ItemVariantId = table.Column<int>(type: "integer", nullable: false),
+                    CharacteristicId = table.Column<int>(type: "integer", nullable: false),
+                    Created = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    CreatedBy = table.Column<int>(type: "integer", nullable: false),
+                    LastModified = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    LastModifiedBy = table.Column<int>(type: "integer", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false),
+                    TenantId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ItemVariantCharacteristic", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ItemVariantCharacteristic_Characteristic_CharacteristicId",
+                        column: x => x.CharacteristicId,
+                        principalTable: "Characteristic",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ItemVariantCharacteristic_ItemVariant_ItemVariantId",
+                        column: x => x.ItemVariantId,
+                        principalTable: "ItemVariant",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ItemVariantCharacteristic_Tenant_TenantId",
+                        column: x => x.TenantId,
+                        principalTable: "Tenant",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItemVariant_ItemId",
+                table: "ItemVariant",
+                column: "ItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItemVariant_TenantId",
+                table: "ItemVariant",
+                column: "TenantId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItemVariantCharacteristic_CharacteristicId",
+                table: "ItemVariantCharacteristic",
+                column: "CharacteristicId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItemVariantCharacteristic_ItemVariantId",
+                table: "ItemVariantCharacteristic",
+                column: "ItemVariantId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItemVariantCharacteristic_TenantId",
+                table: "ItemVariantCharacteristic",
+                column: "TenantId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ItemVariantCharacteristic");
+
+            migrationBuilder.DropTable(
+                name: "ItemVariant");
+
+            migrationBuilder.DropColumn(
+                name: "Code",
+                table: "Item");
+        }
+    }
+}
